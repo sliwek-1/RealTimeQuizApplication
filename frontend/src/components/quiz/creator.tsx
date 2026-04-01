af5399f3ae63e3cf3bd3a362279122ce93f326af
sliwek-1 <msliwinski247@gmail.com>
2026-04-01 17:41:17 +0200

    # creator quiz ui

--- frontend/src/components/quiz/creator.tsx
+++ frontend/src/components/quiz/creator.tsx
@@ -1,27 +1,30 @@
 import React from "react";
-import {Row, Col, Container, Card, ListGroup} from "react-bootstrap";
+import {Row, Col, Container, Card, ListGroup, Button} from "react-bootstrap";
 import "../../css/quiz/creator.css";
 
 export function CreatorMenu() {
     return (
         <>
-            <Container>
-                <Row className="justify-content-between g-4" lg={12}>
-                    <Col xs={12} md={4} lg={2}>
-                        <Card className="border-0">
+            <Container fluid>
+                <Row className="justify-content-between g-4 mt-5" lg={12}>
+                    <Col xs={12} md={4} lg={2} className="left-panel">
+                        <Card className="border-0 w-100 d-flex justify-content-center align-items-center">
                             <Card.Title className="lg">Stwórz test</Card.Title>
-                            <Card.Body>
-                                <ListGroup>
-                                    <ListGroup.Item>Stwórz quiz</ListGroup.Item>
-                                    <ListGroup.Item>Dodaj pytania</ListGroup.Item>
-                                    <ListGroup.Item>Zasady Egzaminu</ListGroup.Item>
-                                    <ListGroup.Item>Ekran Startowy</ListGroup.Item>
-                                    <ListGroup.Item>Ekran Podsumowania</ListGroup.Item>
-                                </ListGroup>
+                            <Card.Body className="w-100">
+                                <div className="tabs">
+                                    <div className="w-100 p-3 rounded shadow-sm">Stwórz quiz</div>
+                                    <div className="w-100 p-3 rounded shadow-sm">Dodaj pytania</div>
+                                    <div className="w-100 p-3 rounded shadow-sm">Zasady Egzaminu</div>
+                                    <div className="w-100 p-3 rounded shadow-sm">Ekran Startowy</div>
+                                    <div className="w-100 p-3 rounded shadow-sm">Ekran Podsumowania</div>
+                                </div>
+                                <div className="button-placeholder border-top">
+                                    <Button variant="success" className="w-100 p-2 mt-3">Aktywuj quiz</Button>
+                                </div>
                             </Card.Body>
                         </Card>
                     </Col>
-                    <Col xs={12} md={8} lg={10}>
+                    <Col xs={12} md={8} lg={10} className="main-panel">
                         <Card className="border-0 shadow-sm">
                             <h1>Stwórz test</h1>
                         </Card>
